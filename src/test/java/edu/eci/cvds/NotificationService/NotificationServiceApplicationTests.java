5d387032875457ec53b894ee7cac68e9d9f024ae
Santiago Guerra <santiago.guerra@mail.escuelaing.edu.co>
2024-10-31 14:47:50 -0500

    Primer commit. Scaffolding inicial del proyecto en Spring

---
+++ src/test/java/edu/eci/cvds/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
