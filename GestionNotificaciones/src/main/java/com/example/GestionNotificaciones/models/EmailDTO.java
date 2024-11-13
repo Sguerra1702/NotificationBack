63c3d44e1671fa589d1347c2bbf711aefd154c85
nataliapaez993@gmail.com <nataliapaez993@gmail.com>
2024-11-13 16:30:38 -0500

    Models

---
+++ GestionNotificaciones/src/main/java/com/example/GestionNotificaciones/models/EmailDTO.java
@@ -0,0 +1,31 @@
+package com.example.GestionNotificaciones.models;
+
+public class EmailDTO {
+    private String responsableEconomico;
+    private String asunto; 
+    private String mensaje;
+
+    public String getResponsableEconomico() {
+        return this.responsableEconomico;
+    }
+
+    public void setResponsableEconomico(String responsableEconomico) {
+        this.responsableEconomico = responsableEconomico;
+    }
+
+    public String getAsunto() {
+        return this.asunto;
+    }
+
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    public String getMensaje() {
+        return this.mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
