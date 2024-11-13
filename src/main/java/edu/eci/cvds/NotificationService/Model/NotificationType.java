766c66d3d16d1163f4ac17e20de06429a7565150
andres3455 <111390187+andres3455@users.noreply.github.com>
2024-11-12 22:59:29 -0500

    Delete src directory

--- src/main/java/edu/eci/cvds/NotificationService/Model/NotificationType.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.cvds.NotificationService.Model;
-
-public enum NotificationType {
-        LOAN_MADE,          //Notificación para el prestamo
-        LOAN_REMINDER,      //Notificación para el recordatorio de vencimiento 
-        LOAN_OVERDUE,       //Notificación para el vencimiento
-        FINE;               //Notificacion de multa
-    
-}
--- src/main/java/edu/eci/cvds/NotificationService/Repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.eci.cvds.NotificationService.Repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import edu.eci.cvds.NotificationService.Model.Notification;
-
-public interface NotificationRepository extends MongoRepository<Notification, Long>{
-
-}
