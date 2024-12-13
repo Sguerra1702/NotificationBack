c1cdeceaa9c501865b8446963c054ea5793dfb13
andres Rodriguez <rodriguezandres160918@gmail.com>
2024-12-13 14:58:41 -0500

    Extraer y convertir datos necesarios del JSON

--- src/main/java/edu/eci/cvds/NotificationService/Controller/EmailController.java
+++ src/main/java/edu/eci/cvds/NotificationService/Controller/EmailController.java
@@ -2,6 +2,8 @@ package edu.eci.cvds.NotificationService.Controller;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 import edu.eci.cvds.NotificationService.Repository.IEmailRepository;
 import edu.eci.cvds.NotificationService.Model.Fines;
 import edu.eci.cvds.NotificationService.Model.Loan;
+import edu.eci.cvds.NotificationService.Model.LoanHistory;
+import edu.eci.cvds.NotificationService.Model.LoanState;
 import edu.eci.cvds.NotificationService.Model.Student;
 import edu.eci.cvds.NotificationService.Service.NotificationService;
 import jakarta.mail.MessagingException;
@@ -33,39 +37,48 @@ public class EmailController {
     // Endpoint para enviar notificación de préstamo realizado
 
     @PostMapping("/loan-made")
-public ResponseEntity<String> endpointNotificacionprestamorealizado(@RequestBody Map<String, Object> loanData) {
+    public ResponseEntity<String> endpointNotificacionprestamorealizado(@RequestBody Map<String, Object> loanData) {
     try {
-        String studentId = (String) loanData.get("studentId");
+        //Extraer solo los necesarios
         String studentName = (String) loanData.get("studentName");
         String bookId = (String) loanData.get("bookId");
         String nameBook = (String) loanData.get("nameBook");
-        LocalDate loanDate = LocalDate.parse((String) loanData.get("loanDate"));
-        LocalDate maxReturnDate = LocalDate.parse((String) loanData.get("maxReturnDate"));
+        String loanStateString = (String) loanData.get("loanState");
+        String loanDate = (String) loanData.get("loanDate");
+        String maxReturnDate = (String) loanData.get("maxReturnDate");
 
-        
-        // Crear un objeto Loan a partir de los datos recibidos
-        Loan loan = new Loan(studentId, studentName, bookId, nameBook, nameBook, loanDate, maxReturnDate, null);
-        
+        // Convertir datos
+        LocalDate loanDateLocal = LocalDate.parse(loanDate);
+        LocalDate maxReturnDateLocal = LocalDate.parse(maxReturnDate);
+        LoanState loanState = LoanState.valueOf(loanStateString.toUpperCase());
+
+        // Crear el objeto Loan
+        Loan loan = new Loan();
+        loan.setStudentName(studentName);
+        loan.setBookId(bookId);
+        loan.setNameBook(nameBook);
+        loan.setLoanState(loanState);
+        loan.setLoanDate(loanDateLocal);
+        loan.setMaxReturnDate(maxReturnDateLocal);
+
         // Verificar si la fecha de vencimiento está dentro de los próximos 3 días
         LocalDate fechaLimiteDevolucion = loan.getMaxReturnDate();
         LocalDate fechaActual = LocalDate.now();
         long diasDeDiferencia = ChronoUnit.DAYS.between(fechaActual, fechaLimiteDevolucion);
 
         if(diasDeDiferencia <= 3) {
-            // Si el préstamo está por vencer, enviar notificación de préstamo por vencer
             notificationService.enviarNotificacionPrestamoPorVencer(loan);
         } else {
-            // De lo contrario, enviar la notificación de préstamo realizado
             notificationService.enviarNotificacionprestamorealizado(loan);
         }
-
         return new ResponseEntity<>("Notificación enviada exitosamente", HttpStatus.OK);
 
     } catch (Exception e) {
-        // Captura cualquier excepción y muestra un mensaje detallado
+        // Capturar cualquier excepción y mostrar un mensaje detallado
         return new ResponseEntity<>("Error en el servidor: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
 
 
 
